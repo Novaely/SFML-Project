a615b32efd0a71350032fa955d9ef7f4947c9c9a
Saele <sarah.oldani1003@gmail.com>
2025-11-25 10:53:44 +0100

    [Add] Bonus Score + Bonus Vie
    Stand by pour le bonus de tir

---
+++ SFML-Project/GameManager.cpp
@@ -0,0 +1,39 @@
+#include "GameManager.h"
+
+
+GameManager::GameManager()
+{
+	_score = 0;
+	_multiplicateur = 1;
+	_timerBonusScore = 0.0f;
+	_timerBonusScoreCheck = 0.0f;
+
+
+}
+	void GameManager::Update(float deltaTime)
+	{
+		_timerBonusScore += deltaTime;
+		if (_timerBonusScore > _timerBonusScoreCheck)
+		{
+			_multiplicateur = 1;
+		}
+	}
+
+
+	void GameManager::BonusScore(float timer, int multiplicateur)
+	{
+		_multiplicateur = multiplicateur;
+		_timerBonusScore = 0;
+		_timerBonusScoreCheck = timer;
+
+	}
+
+	void GameManager::BonusVie(Player & player, float vieRegen)
+	{
+		player.health += vieRegen;
+	}
+
+	void GameManager::BonusTir(Player & player)
+	{
+		// Implementation of shooting direction power-up logic
+	}
--- SFML-Project/GameManager.h
+++ SFML-Project/GameManager.h
@@ -1,8 +1,33 @@
 #pragma once
 #include "Manager.h"
+#include "Player.h"
 
 class GameManager :
     public Manager
 {
+    // Constructors
+    GameManager();
+
+
+    int _score;
+    int _multiplicateur;
+	float _timerBonusScore;
+    float _timerBonusScoreCheck;
+
+    /// <summary>
+    /// score *2 sur x temps
+    /// regen vie
+    /// power up direction de tir 
+    /// </summary>
+
+    void Update(float deltaTime);
+
+    void BonusScore(float timer, int multiplicateur);
+
+	void BonusVie(Player& player, float vieRegen);
+
+    void BonusTir(Player& player);
+
+
 };
 
