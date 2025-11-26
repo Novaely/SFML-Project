70192873b602c116dc59fbaff6357103e861ac5c
Auguste Paccapelo <paccapelo.auguste@gmail.com>
2025-11-26 16:24:11 +0100

    [ADD] Pool System for ShooterEnemy
    # Conflicts:
    #	SFML-Project/PoolManager.cpp
    #	SFML-Project/PoolManager.h

--- SFML-Project/PoolManager.cpp
+++ SFML-Project/PoolManager.cpp
@@ -1,11 +1,17 @@
 #include "PoolManager.h"
 
 PoolManager::PoolManager()
-	: bulletPool(startNumBullets, new Bullet())
+	: bulletPool(startNumBullets, new Bullet()), cacEnemyPool(startNumCACEnemy, new CACEnemy()),
+	shooterEnemyPool(startNumShooterEnemy, new ShooterEnemy())
 {
 	currentNumBullets = startNumBullets;
-	indexOfFirstBullet = 0;
+
+	currentNumCACEnemy = startNumCACEnemy;
+
+	currentNumShooterEnemy = startNumShooterEnemy;
 }
+
+#pragma region Bullets
 
 Bullet* PoolManager::ExtractBullet()
 {
@@ -19,7 +25,7 @@ Bullet* PoolManager::ExtractBullet()
 	return pBullet;
 }
 
-Bullet& PoolManager::GetBullet(Team team, CustomVector2f position, float speed, CustomVector2f direction, float damage)
+Bullet* PoolManager::GetBullet(Team team, CustomVector2f position, float speed, CustomVector2f direction, float damage)
 {
 	Bullet* pBullet = ExtractBullet();
 
@@ -31,21 +37,116 @@ Bullet& PoolManager::GetBullet(Team team, CustomVector2f position, float speed, 
 
 	pBullet->Active();
 
-	return *pBullet;
+	return pBullet;
 }
 
 void PoolManager::AddBulletInPool()
 {
 	currentNumBullets += 10;
-	
+
 	bulletPool.resize(currentNumBullets, new Bullet());
 }
 
-void PoolManager::ReturnBullet(Bullet& bullet)
+void PoolManager::ReturnBullet(Bullet* bullet)
 {
-	bullet.Desactive();
+	bullet->Desactive();
 
 	indexOfFirstBullet--;
 
-	bulletPool[indexOfFirstBullet] = &bullet;
+	bulletPool[indexOfFirstBullet] = bullet;
 }
+
+#pragma endregion
+
+#pragma region CACEnemy
+
+CACEnemy* PoolManager::ExtractCACEnemy()
+{
+	if (indexOfFirstCACEnemy >= currentNumCACEnemy) AddCACEnemyInPool();
+
+	CACEnemy* pEnemy = cacEnemyPool[indexOfFirstCACEnemy];
+
+	cacEnemyPool[indexOfFirstCACEnemy] = nullptr;
+	indexOfFirstCACEnemy++;
+
+	return pEnemy;
+}
+
+CACEnemy* PoolManager::GetCACEnemy(CustomVector2f position, float health, Player* player)
+{
+	CACEnemy* pEnemy = ExtractCACEnemy();
+
+	pEnemy->team = Team::Enemy;
+	pEnemy->position = position;
+	pEnemy->health;
+	// TO DO : Give player
+
+	pEnemy->Active();
+
+	return pEnemy;
+}
+
+void PoolManager::AddCACEnemyInPool()
+{
+	currentNumCACEnemy += 10;
+
+	cacEnemyPool.resize(currentNumCACEnemy, new CACEnemy());
+}
+
+void PoolManager::ReturnEnemy(CACEnemy* enemy)
+{
+	enemy->Desactive();
+
+	indexOfFirstCACEnemy--;
+
+	cacEnemyPool[indexOfFirstCACEnemy] = enemy;
+}
+
+#pragma endregion
+
+#pragma region ShooterEnemy
+
+ShooterEnemy* PoolManager::ExtractShooterEnemy()
+{
+	if (indexOfFirstShooterEnemy >= currentNumShooterEnemy) AddShooterEnemyInPool();
+
+	ShooterEnemy* pEnemy = shooterEnemyPool[indexOfFirstShooterEnemy];
+
+	shooterEnemyPool[indexOfFirstShooterEnemy] = nullptr;
+	indexOfFirstShooterEnemy++;
+
+	return pEnemy;
+}
+
+ShooterEnemy* PoolManager::GetShooterEnemy(CustomVector2f position, float health, Player* player)
+{
+	ShooterEnemy* pEnemy = ExtractShooterEnemy();
+
+	pEnemy->team = Team::Enemy;
+	pEnemy->position = position;
+	pEnemy->health;
+	// TO DO : Give player
+
+	pEnemy->Active();
+
+	return pEnemy;
+}
+
+void PoolManager::AddShooterEnemyInPool()
+{
+	currentNumShooterEnemy += 10;
+
+	shooterEnemyPool.resize(currentNumShooterEnemy, new ShooterEnemy());
+}
+
+void PoolManager::ReturnEnemy(ShooterEnemy* enemy)
+{
+	enemy->Desactive();
+
+	indexOfFirstShooterEnemy--;
+
+	shooterEnemyPool[indexOfFirstShooterEnemy] = enemy;
+}
+
+#pragma endregion
+
--- SFML-Project/PoolManager.h
+++ SFML-Project/PoolManager.h
@@ -1,23 +1,48 @@
 #pragma once
 
 #include "Manager.h"
-#include "Bullet.h"
 
 class PoolManager : public Manager
 {
 	public:
 		PoolManager();
-		Bullet& GetBullet(Team team, CustomVector2f position, float speed, CustomVector2f direction, float damage);
 
-		void ReturnBullet(Bullet& bullet);
+		Bullet* GetBullet(Team team, CustomVector2f position, float speed, CustomVector2f direction, float damage);
+		CACEnemy* GetCACEnemy(CustomVector2f position, float health, Player* player);
+		ShooterEnemy* GetShooterEnemy(CustomVector2f position, float health, Player* player);
+
+		void ReturnBullet(Bullet* bullet);
+		void ReturnEnemy(CACEnemy* enemy);
+		void ReturnEnemy(ShooterEnemy* enemy);
 
 	private:
+		// Bullets
 		int startNumBullets = 100;
 
-		int indexOfFirstBullet;
+		int indexOfFirstBullet = 0;
 		int currentNumBullets;
 		std::vector<Bullet*> bulletPool;
 
 		Bullet* ExtractBullet();
 		void AddBulletInPool();
+
+		// CACEnemy
+		int startNumCACEnemy = 20;
+
+		int indexOfFirstCACEnemy = 0;
+		int currentNumCACEnemy;
+		std::vector<CACEnemy*> cacEnemyPool;
+
+		CACEnemy* ExtractCACEnemy();
+		void AddCACEnemyInPool();
+
+		// ShooterEnemy
+		int startNumShooterEnemy = 20;
+
+		int indexOfFirstShooterEnemy = 0;
+		int currentNumShooterEnemy;
+		std::vector<ShooterEnemy*> shooterEnemyPool;
+		
+		ShooterEnemy* ExtractShooterEnemy();
+		void AddShooterEnemyInPool();
 };
