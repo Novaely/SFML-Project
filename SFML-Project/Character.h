2f174934e2c89c9b04850355c85ec0ebd50b0699
Saele <sarah.oldani1003@gmail.com>
2025-12-05 16:42:50 +0100

    [Fix] Protected declaration

--- SFML-Project/Character.h
+++ SFML-Project/Character.h
@@ -3,29 +3,29 @@
 
 class Character : public Movable
 {
-	public : 
-		Character();
-		float health = 0;
-		float speedBullet = 0;
-		float timerShootAgain = 0;
-		float chronoShootAgain = 0;
+public:
+	Character();
+	float health = 0;
+	float speedBullet = 0;
+	float timerShootAgain = 0;
+	float chronoShootAgain = 0;
 
-		float acceleration = 0;
-		float stopFriction = 0;
-		float turnBackFriction = 0;
-		float rotationSpeed = 0;
-		float rotationDirection = 0;
+	float acceleration = 0;
+	float stopFriction = 0;
+	float turnBackFriction = 0;
+	float rotationSpeed = 0;
+	float rotationDirection = 0;
 
-		CustomVector2f bulletSpawnPos = CustomVector2f::zero;
+	CustomVector2f bulletSpawnPos = CustomVector2f::zero;
 
-		bool CanShoot() const;
-		void Update(float deltaTime) override;
-		void Move(float deltaTime) override;
-		void Rotate(float deltaTime);
+	bool CanShoot() const;
+	void Update(float deltaTime) override;
+	void Move(float deltaTime) override;
+	void Rotate(float deltaTime);
 
-		CustomVector2f GetLookDirection();
+	CustomVector2f GetLookDirection();
 
-	protected:
-		bool _canShoot = true;
-		virtual void Damage(float dmg);
+protected:
+	bool _canShoot = true;
+	virtual void Damage(float dmg);
 };
--- SFML-Project/Player.h
+++ SFML-Project/Player.h
@@ -5,6 +5,7 @@ class Player : public Character
 {
 private:
     int _levelShooter = 1;
+protected:
     void Damage(float dmg) override;
 
 public:
