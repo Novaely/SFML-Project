b0326ed9d21a6c8acdb64388a7deedca2d882b72
Auguste Paccapelo <paccapelo.auguste@gmail.com>
2025-12-07 14:58:27 +0100

    [UPDATE] Support TurretEnemy

--- SFML-Project/PoolManager.cpp
+++ SFML-Project/PoolManager.cpp
@@ -191,6 +191,61 @@ void PoolManager::ReturnEnemy(ShooterEnemy* enemy)
 
 #pragma endregion
 
+#pragma region TurretEnemy
+
+TurretEnemy* PoolManager::ExtractTurretEnemy()
+{
+	if (indexOfFirstTurretEnemy >= currentNumTurretEnemy) AddTurretEnemyInPool();
+
+	TurretEnemy* pEnemy = turretEnemyPool[indexOfFirstTurretEnemy];
+
+	turretEnemyPool[indexOfFirstTurretEnemy] = nullptr;
+	indexOfFirstTurretEnemy++;
+
+	return pEnemy;
+}
+
+TurretEnemy* PoolManager::GetTurretEnemy(CustomVector2f position, float health, Player* player)
+{
+	TurretEnemy* pEnemy = ExtractTurretEnemy();
+
+	pEnemy->team = Team::Enemy;
+	pEnemy->position = position;
+	pEnemy->shape->setPosition(position.x, position.y);
+
+	pEnemy->health;
+
+	pEnemy->targetPos = &(player->position);
+
+	pEnemy->Active();
+
+	return pEnemy;
+}
+
+void PoolManager::AddTurretEnemyInPool()
+{
+	currentNumTurretEnemy += 10;
+
+	turretEnemyPool.resize(currentNumTurretEnemy, nullptr);
+
+	for (int i = indexOfFirstTurretEnemy; i < currentNumTurretEnemy; i++)
+	{
+		turretEnemyPool[i] = new TurretEnemy();
+	}
+}
+
+void PoolManager::ReturnEnemy(TurretEnemy* enemy)
+{
+	enemy->Desactive();
+
+	indexOfFirstTurretEnemy--;
+
+	turretEnemyPool[indexOfFirstTurretEnemy] = enemy;
+}
+
+#pragma endregion
+
+
 PoolManager::~PoolManager()
 {
 	for (int i = 0; i < currentNumBullets; i++)
