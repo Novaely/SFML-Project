5ab48fac4565a5a3f8b23fad2911de9ee78605c4
Saele <sarah.oldani1003@gmail.com>
2025-12-10 15:27:27 +0100

    [Add] Player invincible apres degats + clignotement

--- SFML-Project/Player.h
+++ SFML-Project/Player.h
@@ -7,12 +7,19 @@ class Player : public Character
 private:
     int _levelShooter = 1;
     int _levelShooterMax = 3;
+    bool _isInvicible = false;
+    float _invicibleTime = 2.0f;
+	float _timeSinceInvincible = 0.0f;
+	float _timeSinceColorChange = 0.0f;
 protected:
     void Damage(float dmg) override;
 
 public:
     //constructer
     Player();
+
+
+	void Update(float deltaTime) override;
 
     int GetlevelShooter() const;
     void SetlevelShooter(int val);
