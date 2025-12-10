bc80252d2f7b4ed583a82ba1b1343fa3de6a1420
Saele <sarah.oldani1003@gmail.com>
2025-12-10 17:26:46 +0100

    [Add] End Screen

--- SFML-Project/GameManager.cpp
+++ SFML-Project/GameManager.cpp
@@ -61,6 +61,10 @@ void GameManager::Update(float deltaTime, CustomVector2f windowSize)
 	{
 		_multiplicateur = 1;
 	}
+	if(player->health == 0)
+	{
+		pause = true;
+	}
 }
 
 void GameManager::Draw(sf::RenderWindow& window)
